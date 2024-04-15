5a7a72dd7550b55ddd86ea2df5ef70e97164b72f
Aakashsaini2410 <aakashsaini2324@gmail.com>
2024-04-15 22:10:50 +0530

    yes

---
+++ ifelse.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int n;
+    cin>>n;
+    //cout<<"value of n is "endl;
+
+    if (n>0){
+        cout<<"A is possitive"<<endl;
+    }
+    else{
+        cout<<"A is negative"<<endl;   
+    }*/
+
+int a,b;
+    cout<<"enter the value of a"<<endl;
+    cin>>a;
+    cout<<"enter the value of b"<<endl;
+    cin>>b;
+
+    if(a>b){
+        cout<<"A is greater"<<endl;
+    }
+    if(b>a){
+        cout<<"B is greater"<<endl;
+
+    }
+    }
+
---
+++ loop.cpp
@@ -0,0 +1,17 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     int i;
+//     for(i=1;i<=5;i++)
+//         cout<<i;
+// }
+
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i = 0;i<5;i=i+1)
+    cout <<"A"<<" ";
+
+}   
+    cout << endl;
